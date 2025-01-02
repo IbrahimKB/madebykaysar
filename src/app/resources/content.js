bbedd5689242e13af1d27351e2ebcd14c946431f
IbrahimKB <120331575+IbrahimKB@users.noreply.github.com>
2025-01-02 19:21:33 +0000

    name change

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,8 +1,8 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
+    firstName: 'Kaysar',
+    lastName:  '',
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
